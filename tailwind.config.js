b2f6033fcc54fcc6cf261e20801c6fa6e5c47e91
Éric <nascimento3ric@gmail.com>
2024-02-07 23:40:30 -0300

    Corrige erro de estilo tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-  ],
+    "./src/**/*.{html, ts}",
+  ],  
   theme: {
     extend: {
       fontFamily: {
