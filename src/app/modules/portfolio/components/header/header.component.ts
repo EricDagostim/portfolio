3a47dbc2a4ee4ee38888a2bb4857b1c7bab7b406
Éric <nascimento3ric@gmail.com>
2024-02-10 10:31:46 -0300

    Adiciona badged de copiado em interação com botão de email

--- src/app/modules/portfolio/components/header/header.component.ts
+++ src/app/modules/portfolio/components/header/header.component.ts
@@ -1,14 +1,25 @@
+
 import { Component } from '@angular/core';
+import { fadeInAnimation } from '../../animations';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+  ],
+  animations: [
+    fadeInAnimation
+  ],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  public badgeState = "hidden"
 
-  copyEmail(){
-    console.log("copiado")
+  copyEmail() {
+    this.badgeState = "block"
+
+    setTimeout(() => {
+      this.badgeState = "hidden"
+    }, 2000);
   }
 }
