e50aa0991e431808300fb7a5499f0a75260d5963
Éric <nascimento3ric@gmail.com>
2024-02-10 11:15:52 -0300

    Adiciona interação com botão de email

--- src/app/modules/portfolio/components/header/header.component.ts
+++ src/app/modules/portfolio/components/header/header.component.ts
@@ -1,11 +1,14 @@
 
 import { Component } from '@angular/core';
 import { fadeInAnimation } from '../../animations';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
+    CommonModule,
   ],
   animations: [
     fadeInAnimation
@@ -13,13 +16,24 @@ import { fadeInAnimation } from '../../animations';
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  public badgeState = "hidden"
+  public badgeState: string = "hidden";
+  public animationState: string = ":enter";
+
+
+  constructor() { }
+
 
   copyEmail() {
     this.badgeState = "block"
+    console.log("Email copied to clipboard")
+
+
+    setTimeout(() => {
+      this.animationState = ":leave"
+    }, 2000);
 
     setTimeout(() => {
       this.badgeState = "hidden"
-    }, 2000);
+    }, 2300);
   }
 }
