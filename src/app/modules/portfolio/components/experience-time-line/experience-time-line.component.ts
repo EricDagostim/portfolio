5e14622400063a8b46b53d7f0791c6befb35f775
Éric Dagostim do Nascimento <69852409+EricDagostim@users.noreply.github.com>
2026-03-18 02:19:28 +0000

    Feat/ Atualizando informações de perfil 2026

--- src/app/modules/portfolio/components/experience-time-line/experience-time-line.component.ts
+++ src/app/modules/portfolio/components/experience-time-line/experience-time-line.component.ts
@@ -4,16 +4,24 @@ import { Component } from '@angular/core';
   selector: 'app-experience-time-line',
   standalone: true,
   imports: [],
-  templateUrl: './experience-time-line.component.html'  
+  templateUrl: './experience-time-line.component.html'
 })
 export class ExperienceTimeLineComponent {
 
   public experiences = [
     {
+      office: 'Desenvolvedor Mobile',
+      company: 'Useall Software',
+      companyLink: 'https://useall.com.br/',
+      date: 'Maio de 2024 - Atualmente',
+      description: 'Desenvolvedor Mobile atuando em aplicações críticas de larga escala para coleta e faturamento de energia elétrica em âmbito nacional. Responsável por iniciativas de modernização tecnológica, incluindo migração de plataformas, reescrita de sistemas legados e melhoria contínua da arquitetura. Implementação de testes automatizados e evolução de pipelines CI/CD, elevando a confiabilidade, performance e qualidade das entregas.',
+      tools: 'C#, .NET Framework, .NET MAUI, Android, JavaScript, ExtJs, Oracle SQL e Azure Devops.'
+    },
+    {
       office: 'Desenvolvedor Front-End',
       company: 'Prefeitura Municipal de Criciúma',
       companyLink: 'https://www.criciuma.sc.gov.br/',
-      date: 'Outubro de 2022 - Hoje',
+      date: 'Outubro de 2022 - Maio de 2024',
       description: 'Cargo efetivo de 8h, atuando no desenvolvimento de portais e sistemas web de alta performance, levantamento de requisitos, criação de interfaces para protótipos de validação de requisitos (figma), metodologia kanban e desenvolvimento front-end. Desenvolvimento de sites e portais acessíveis, responsivos e performáticos que atendam as necessidades dos munícipes.',
       tools: 'HTML, CSS, JavaScript, Jquery, PHP, CakePHP, Bootstrap, Google Analytics e Figma.'
     },
@@ -32,7 +40,7 @@ export class ExperienceTimeLineComponent {
       date: 'Setembro de 2021 - Março de 2022',
       description: 'Estágio de 6 horas diárias, atuando no atendimento service-desk e suporte técnico, manutenção de computadores, instalação e configuração de softwares, manutenção de redes e servidores, suporte a sistemas de gestão',
       tools: ''
-    }    
+    }
   ];
 
 }
